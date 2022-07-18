47a7550f101a2b727bea88a3efc90010a3860816
raz1boo <pavlik.shevchenko96@gmail.com>
2022-07-18 19:27:32 +0300

    123

--- pixema-app/src/components/Auth/NewPassword/NewPassword.tsx
+++ pixema-app/src/components/Auth/NewPassword/NewPassword.tsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
 import Input from "../../api/AuthInput/Input";
 import Submit from "../../api/AuthInput/Submit";
-import Logo from "../../Header/Logo/Logo";
-=======
 import Logo from "../../UI/Header/Logo/Logo";
->>>>>>> 99449ca7d15372e8b7a9689e867c00b31e2f82e4
 import "./NewPassword.scss";
 
 const NewPassword = () => {
