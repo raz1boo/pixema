6dd524ab58502c7525845cadee3d879246f1676d
konovalchuk <konoval_chuk@mail.ru>
2022-08-17 14:28:04 +0300

    login validation is ready

--- src/components/Auth/Login/Login.tsx
+++ src/components/Auth/Login/Login.tsx
@@ -1,5 +1,5 @@
-import Input from "../AuthInput/Input";
-import Submit from "../AuthInput/Submit";
+// import Input from "../AuthInput/Input";
+// import Submit from "../AuthInput/Submit";
 import Logo from "../../UI/Header/Logo/Logo";
 import "./Login.scss";
 import "../../Auth/Authorization.scss";
@@ -7,31 +7,50 @@ import { useAppSelector } from "../../store/hooks/redux";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useCreateTokenMutation } from "../../requests/authorization";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { ILogin } from "./Login.interface";
+import { useLocation } from "react-router-dom";
 
 const Login = () => {
   const { theme } = useAppSelector((state) => state.themeReducer);
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const location = useLocation();
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
   const [createToken, { data }] = useCreateTokenMutation();
 
-  const changeEmail = (email: string) => {
-    setEmail(email);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ILogin>(
+    { mode: "onChange" } //Ошибка срабатывает при изменении поля
+  );
+
+  const onSubmit: SubmitHandler<ILogin> = (data) => {
+    alert(`your email ${data.email}`);
+    alert(`your password ${data.password}`); //Отправить на сервер
+    reset();
   };
 
-  const changePassword = (password: string) => {
-    setPassword(password);
-  };
-  const authUser = async () => {
-    await createToken({ email, password })
-      .unwrap()
-      .then((data) => data && navigate("/", { replace: true }));
-  };
+  // const changeEmail = (email: string) => {
+  //   setEmail(email);
+  // };
 
-  useEffect(() => {
-    document.cookie = `access=${data ? data?.access : ""}`;
-    document.cookie = `refresh=${data ? data?.refresh : ""}`;
-  }, [data]);
+  // const changePassword = (password: string) => {
+  //   setPassword(password);
+  // };
+  // const authUser = async () => {
+  //   await createToken({ email, password })
+  //     .unwrap()
+  //     .then((data) => data && navigate("/", { replace: true }));
+  // };
+
+  // useEffect(() => {
+  //   document.cookie = `access=${data ? data?.access : ""}`;
+  //   document.cookie = `refresh=${data ? data?.refresh : ""}`;
+  // }, [data]);
+
   return (
     <>
       <div className="login-logo">
@@ -39,6 +58,7 @@ const Login = () => {
       </div>
       <section className="section-window">
         <form
+          onSubmit={handleSubmit(onSubmit)}
           action=""
           className="form-window login-form-window"
           style={
@@ -54,8 +74,98 @@ const Login = () => {
           >
             Вход
           </h2>
-          {/* <h3>Ваш пароль успешно изменён!</h3> */}
-          <Input
+          <label
+            style={{
+              color:
+                theme === "dark" || location.pathname === "/"
+                  ? "#fff"
+                  : "#242426",
+            }}
+          >
+            Почта
+          </label>
+          <input
+            style={
+              theme === "dark" || location.pathname === "/"
+                ? { backgroundColor: "#323537", borderColor: "transparent" }
+                : {
+                    backgroundColor: "#fff",
+                    borderColor: "#AFB2B6",
+                    color: "#000",
+                  }
+            }
+            {...register("email", {
+              required: "Почта не может быть пустой",
+              pattern: {
+                value:
+                  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+                message: "Некорректная почта",
+              },
+            })}
+            type="text"
+            placeholder="Введите почту"
+            // value={email}
+            // onChange={(event) => changeEmail(event.target.value)}
+          />
+
+          {errors.email && (
+            <div style={{ color: "#ed4337" }}>{errors.email.message}</div>
+          )}
+
+          <label
+            style={{
+              color:
+                theme === "dark" || location.pathname === "/"
+                  ? "#fff"
+                  : "#242426",
+            }}
+          >
+            Пароль
+          </label>
+          <input
+            style={
+              theme === "dark" || location.pathname === "/"
+                ? { backgroundColor: "#323537", borderColor: "transparent" }
+                : {
+                    backgroundColor: "#fff",
+                    borderColor: "#AFB2B6",
+                    color: "#000",
+                  }
+            }
+            {...register("password", {
+              required: "Пароль не может быть пустым",
+              pattern: {
+                value: /(?=.*[0-9])(?=.*[a-zA-Z]).{8,30}/,
+                message: "Пароль должен состоять из букв латинского алфавита (A-z) и арабских цифр (0-9)",
+              },
+            })}
+            type="password"
+            placeholder="Введите пароль"
+            // value={password}
+            // onChange={(event) => changePassword(event.target.value)}
+          />
+
+          {errors.password && (
+            <div style={{ color: "#ed4337" }}>{errors.password.message}</div>
+          )}
+
+          <Link to="/reset_password">Забыли пароль?</Link>
+
+          <input
+            className="submit"
+            type="submit"
+            value="Войти"
+            // onClick={authUser}
+          />
+          <p
+            style={{
+              color: theme === "dark" ? "#fff" : "#80858b",
+            }}
+          >
+            У Вас нет аккаунта? <Link to="/registration">Регистрация</Link>
+          </p>
+
+          {/* <Input
             label="Почта"
             type="email"
             name="email"
@@ -77,14 +187,7 @@ const Login = () => {
             type="submit"
             value="Войти"
             onClick={authUser}
-          />
-          <p
-            style={{
-              color: theme === "dark" ? "#fff" : "#80858b",
-            }}
-          >
-            У Вас нет аккаунта? <Link to="/registration">Регистрация</Link>
-          </p>
+          /> */}
         </form>
       </section>
     </>
